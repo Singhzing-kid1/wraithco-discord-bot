8055ba69e640c1e0c07cb4527b04bc6aa02e7e03
Singhzing-Kid1 <veer.singh.real@pm.me>
2026-08-06 18:33:26 -0600

    set proper gateway intents for prefix commands

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,20 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 #[tokio::main]
 async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
-    let intents = serenity::GatewayIntents::non_privileged();
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
+            commands: vec![commands::age(), commands::colour()],
             prefix_options: poise::PrefixFrameworkOptions {
                 prefix: Some(std::env::var("PREFIX").expect("missing PREFIX").into()),
-                edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(std::time::Duration::from_secs(3600)))),
+                edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                    std::time::Duration::from_secs(3600),
+                ))),
                 case_insensitive_commands: true,
                 ..Default::default()
             },
-            commands: vec![commands::age(), commands::colour()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
