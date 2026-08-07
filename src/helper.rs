703fcbffac7937d7de179ad8790c1d0eaa197b8b
Singhzing-Kid1 <veer.singh.real@pm.me>
2026-08-06 21:39:44 -0600

    tells user if they arent of the correct role to run the command

--- src/helper.rs
+++ src/helper.rs
@@ -8,5 +8,16 @@ pub(crate) async fn is_board_of_directors(ctx: Context<'_>) -> Result<bool, Erro
 
     let has_role = member.has_role(ctx, ctx.guild_id().unwrap(), board_of_directors).await?;
 
+    if !has_role {
+        let guild_id = ctx.guild_id().unwrap();
+        let roles = guild_id.roles(ctx.http()).await?;
+
+        let role = roles.get(&board_of_directors).map(|r| r.name.clone()).unwrap();
+
+
+        let response = format!("you need {} to be able to run this command", role);
+        ctx.say(response).await?;
+    }
+
     Ok(has_role)
 }
