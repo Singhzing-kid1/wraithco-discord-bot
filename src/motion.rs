915ff7c619c0c9955bbcf2a883fa899fc2bf64e6
Singhzing-Kid1 <veer.singh.real@pm.me>
2026-08-07 18:24:08 -0600

    author can close the motion before voting (does not delete the motion from the forum channel and the motion will not be enacted if closed before voting)

--- src/motion.rs
+++ src/motion.rs
@@ -6,25 +6,45 @@ use poise::serenity_prelude::{self as serenity, Mentionable};
 pub async fn create(
     ctx: Context<'_>,
     #[description = "Title of the motion"] title: String,
-    #[description = "motion details"] body: String,
+    #[description = "motion details"] details: String,
 ) -> Result<(), Error> {
     let forum_id: serenity::ChannelId = std::env::var("MOTIONS_CHANNEL")
         .expect("missing MOTIONS_CHANNEL")
         .parse()?;
 
-    let content = format!("Author: {} \n Date: {} \n Details: \n {}", ctx.author().mention(), ctx.created_at().to_utc(), body);
+    let content = format!(
+        "Author: {}\nDate: {}\nDetails:\n {}",
+        ctx.author().mention(),
+        ctx.created_at().to_utc(),
+        details
+    );
     let initial_message = serenity::CreateMessage::new().content(content);
 
-
-    let builder =
-        serenity::CreateForumPost::new(title, initial_message);
+    let builder = serenity::CreateForumPost::new(title, initial_message);
 
     let new_post = forum_id.create_forum_post(ctx.http(), builder).await?;
-    
-    new_post.id.pin(ctx.http(), serenity::MessageId::new(new_post.id.get())).await?;
+
+    new_post
+        .id
+        .pin(ctx.http(), serenity::MessageId::new(new_post.id.get()))
+        .await?;
 
     ctx.say(format!("Motion Created: {}", new_post.mention()))
         .await?;
 
+    Ok(())
+}
+
+
+#[poise::command(slash_command, check = "helper::is_board_of_directors")]
+pub async fn close(ctx: Context<'_>, motion: serenity::Channel) -> Result<(), Error> {
+    let content = format!("the motion ({}) has been closed before voting.", motion.mention());
+
+    let message_builder = serenity::CreateMessage::new().content(content);
+
+    let message = motion.id().send_message(ctx.http(), message_builder).await?;
+
+    message.pin(ctx.http()).await?;
+    
     Ok(())
 }
