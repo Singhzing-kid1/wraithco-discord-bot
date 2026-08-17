fa424f5870cff7973840c85f6e73db5e635d3780
Singhzing-Kid1 <veer.singh.real@pm.me>
2026-08-16 19:02:02 -0600

    almost forgot to register command whoops

--- src/commands.rs
+++ src/commands.rs
@@ -13,7 +13,7 @@ pub async fn motion(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
-#[poise::command(slash_command, subcommands("vote::start"))]
+#[poise::command(slash_command, subcommands("vote::start", "vote::end"))]
 pub async fn vote(_ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
